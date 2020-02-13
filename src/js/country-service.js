7900be358401cbd5271f5342d991a224f14039a9
Mashina <mi.sidorenko@gmail.com>
2020-02-13 16:20:31 +0200

    added_Pnotify

--- src/js/country-service.js
+++ src/js/country-service.js
@@ -3,20 +3,31 @@ import articleListItemTamplate from "../templates/article-list-item.hbs";
 import countryList from "../templates/item-coutries.hbs";
 import fetchCountry from "./services/services";
 import { debounce } from "debounce";
-
-
+import PNotify from 'pnotify/dist/es/PNotify';;
+import PNotifyButtons from 'pnotify/dist/es/PNotifyStyleMaterial';
 
 const refs = {
   searchInput: document.querySelector(".js-search-input"),
   articleList: document.querySelector(".article-list"),
   boxCountry: document.querySelector(".list-country")
-
 };
+
 refs.searchInput.addEventListener("input", debounce(searchInputValue, 500));
 
+// function searchInputValue(e) {
+//   const input = e.target.value; 
+//   generalFetch(input)
+  
+// };
+
 function searchInputValue(e) {
+  e.preventDefault();
   const input = e.target.value; 
-  generalFetch(input) 
+  clearListItems();
+  fetchCountry(input)
+  .then(country => {
+    insertListItems(country)
+  }); 
 };
 
 function insertListItems(items) {
@@ -30,41 +41,34 @@ function insertList(item){
 
 function clearListItems() {
   refs.articleList.innerHTML = '';
-  refs.boxCountry.innerHTML = ""
+  refs.boxCountry.innerHTML = "";
 };
 
-function reanderCountry (country){
-if(country.length >=2 && country.length<=10 ){
-  insertListItems(country)
-} else if(country.length === 1){
-  insertList(country)
-} else if (country.length>10){
-  alert("Nooooo!")
-} else{
-  alert("IO")
-}
-};
+// function reanderCountry (country){
+// if(country.length >=2 && country.length<=10 ){
+//   insertListItems(country);
+// } else if(country.length === 1){
+//   insertList(country)
+// } else if (country.length>10){
+//   PNotify.error({
+//     title: 'Desktop Error',
+//     text: 'Too many matches found. Pleace enter a more specific query'   
+//   });
+// } else{
+//   PNotify.error({
+//     title: 'Desktop Error',
+//     text: 'Can not found you query'   
+//   });
+// }
+// };
 
-function generalFetch (countryName) {
-  clearListItems();
-  if (countryName === "")
-  {
-    return;
-  }
-fetchCountry(countryName)
-.then(countries=>reanderCountry(countries))
-.catch(error=>console.warn(console.error));
-};
-
-
-
-
-// function searchFormSubmit(e) {
-//   e.preventDefault();
-//   const input = e.target.value; 
+// function generalFetch (countryName) {
 //   clearListItems();
-//   fetchCountry(input)
-//   .then(country => {
-//     insertListItems(country)
-//   }); 
+//   if (countryName === "")
+//   {
+//     return;
+//   }
+// fetchCountry(countryName)
+// .then(countries=>reanderCountry(countries))
+// .catch(error=>console.warn(console.error));
 // };
